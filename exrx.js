4e46fc03764fc355b8542939ba2e3e3b83ab0bc9
Ian Voysey <iev@cs.cmu.edu>
2017-04-17 12:21:49 -0400

    working, need to make it a UL

--- exrx.js
+++ exrx.js
@@ -43,5 +43,24 @@ ex = ["band walking",
       "wall sits",
       "windmills"]
 
-const shuffled = ex.sort(() => .5 - Math.random());
-let selected = shuffled.slice(0,10);
+
+function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+}
+
+document.getElementById('exlist').innerHTML = (ex.sort(() => .5 - Math.random())).slice(0,10);
